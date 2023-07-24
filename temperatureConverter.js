c1fd09ff81037b4180a2c0caaf9c30eddf571716
Colin Jaffe <balloonasaurus@gmail.com>
2023-07-24 15:51:41 -0400

    Adjusts for new test runner API, finishes many tests, and adds solution.

--- temperatureConverter.js
+++ temperatureConverter.js
@@ -18,3 +18,10 @@ function getDegrees(userInput) {
   return Number(userInput.slice(0, -1));
 }
 
+function convertCToF(degreesC) {
+  return degreesC / 5 * 9 + 32;
+}
+
+function convertFToC(degreesF) {
+  return (degreesF - 32) / 9 * 5;
+}
